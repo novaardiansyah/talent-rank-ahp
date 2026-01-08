9971b375f34e05f8c968ebd61d430ffe2bb227b5
novaardiansyah <novaardiansyah78@gmail.com>
2026-01-08 16:27:05 +0700

    feat(AlternativeComparison): add reset weights feature
    - Implement reset weights action in ListAlternativeComparisons
    - Truncate and re-initialize alternative comparisons for all criteria and alternatives

--- app/Filament/Resources/AlternativeComparisons/Pages/ListAlternativeComparisons.php
+++ app/Filament/Resources/AlternativeComparisons/Pages/ListAlternativeComparisons.php
@@ -3,17 +3,61 @@
 namespace App\Filament\Resources\AlternativeComparisons\Pages;
 
 use App\Filament\Resources\AlternativeComparisons\AlternativeComparisonResource;
-use Filament\Actions\CreateAction;
+use App\Models\Alternative;
+use App\Models\AlternativeComparison;
+use App\Models\Criteria;
+use Carbon\Carbon;
+use Filament\Actions\Action;
+use Filament\Notifications\Notification;
 use Filament\Resources\Pages\ListRecords;
 
 class ListAlternativeComparisons extends ListRecords
 {
-    protected static string $resource = AlternativeComparisonResource::class;
+  protected static string $resource = AlternativeComparisonResource::class;
 
-    protected function getHeaderActions(): array
-    {
-        return [
-            CreateAction::make(),
-        ];
-    }
+  protected function getHeaderActions(): array
+  {
+    return [
+      Action::make('reset')
+        ->label('Reset Bobot')
+        ->icon('heroicon-s-arrow-path')
+        ->color('primary')
+        ->requiresConfirmation()
+        ->modalHeading('Reset bobot alternatif')
+        ->action(function (Action $action) {
+          AlternativeComparison::truncate();
+
+          $criterias = Criteria::all();
+          $alternatives = Alternative::all();
+
+          $temp = [];
+          $now = Carbon::now();
+
+          foreach ($criterias as $criteria) {
+            foreach ($alternatives as $alternative) {
+              foreach ($alternatives as $alternative2) {
+                $temp[] = [
+                  'criterion_id'     => $criteria->id,
+                  'alternative_id_1' => $alternative->id,
+                  'alternative_id_2' => $alternative2->id,
+                  'value'            => 1,
+                  'created_at'       => $now,
+                  'updated_at'       => $now,
+                ];
+              }
+            }
+          }
+
+          AlternativeComparison::insert($temp);
+
+          $action->success();
+          $action->successNotification(
+            Notification::make()
+              ->success()
+              ->title('Berhasil reset bobot alternatif')
+              ->body('Seluruh bobot alternatif berhasil direset kembali ke default')
+          );
+        })
+    ];
+  }
 }
